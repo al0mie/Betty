5bf13f1872ccc5d59ed821e60e1f29f5e8f1f218
al0mie <alex.mokrencko@gmail.com>
2017-04-14 13:49:54 +0300

    add bootstrap file for, process handling routes from file

---
+++ bootstrap/Bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/Router.php';
+
+$uri = Router::makeUri();
+
+// Check uri and compare with routes
+if ($params = Router::matchUri($uri)) {
+    $controller = 'App\Controllers\\' . ucwords($params['controller']) . 'Controller';
+    $method = $params['method'];
+
+    unset($params['controller'], $params['method']);
+
+    if (class_exists($controller)) {
+        if (method_exists($controller, $method)) {
+            $controller = new $controller();
+            $controller->$method($params);
+        } else {
+            throw new Exception('Bootstrap : No method found ' . $method);
+        }
+    } else {
+        throw new Exception('Bootstrap : No controller found ' . $controller);
+    }
+} else {
+    throw new Exception('Bootstrap : No route found ' . $uri);
+}
