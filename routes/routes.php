a313714fc399c9638ce421ad0e912befa7db5e86
al0mie <alex.mokrencko@gmail.com>
2017-04-14 14:04:14 +0300

    move routes to separate file

---
+++ routes/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+$routes[''] = [
+    '',
+    'controller' => 'Betting',
+    'method' => 'index',
+];
+
+$routes['showScore'] = [
+    'showScore',
+    'controller' => 'Betting',
+    'method' => 'showScoreForm',
+];
+
+$routes['processGame'] = [
+    'processGame',
+    'controller' => 'Betting',
+    'method' => 'processGame',
+];
+
+$routes['updateScore'] = [
+    'updateScore',
+    'controller' => 'Betting',
+    'method' => 'updateScore',
+];
