bd1bad788b98c6321322616e383c30589f4e455e
al0mie <alex.mokrencko@gmail.com>
2017-04-14 14:30:11 +0300

    unit tests for creating bets

--- tests/BettingTest.php
+++ tests/BettingTest.php
@@ -32,4 +32,17 @@ class MyGuestbookTest extends TestCase
     {
         return $this->createFlatXMLDataSet(dirname(__FILE__) . '/seeds/bettings-seed.xml');
     }
+
+    public function testCreateBet()
+    {
+        $this->assertEquals(1, $this->getConnection()->getRowCount('bettings'), "Pre-Condition");
+        $this->bettingService->createBet(['player_id' => 'user 1', 'amount' => 20, 'game_id' => 1]);
+        $this->assertEquals(2, $this->getConnection()->getRowCount('bettings'), "After insert");
+    }
+
+    public function testCreateNotValidBet()
+    {
+        $this->bettingService->createBet(['player_id' => 'user 1', 'amount' => 20, 'game_id' => 1]);
+        $this->assertEquals(2, $this->getConnection()->getRowCount('bettings'), "After insert");
+    }
 }
